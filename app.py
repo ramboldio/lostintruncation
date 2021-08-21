85903f4a5077ac4c861d7cd8357994bc47fd5d25
Lukas Rambold <lukas@rambold.io>
2021-08-21 19:23:51 +0200

    fixed

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from time import sleep
 import serial
 from PIL import Image
 from flask_cors import CORS, cross_origin
-from werkzeug.utils import secure_filename	
+from werkzeug.utils import secure_filename  
 
 photo_printer_name = 'SELPHY-CP1300'
 
@@ -17,37 +17,37 @@ CORS(app)
 cups_connection = cups.Connection()
 
 @app.route('/submit', methods = ['POST'])
-def submit(image_file):
+def submit():
     if request.method == 'POST':
-		file = request.files['file']
-   		if file and allowed_file(file.filename):
+        file = request.files['file']
+        if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             image_path = os.path.join('./uploaded_images', filename)
             file.save(image_path)
             print_image(image_path)
 
 def print_image(image_path='test.jpg'):
-	# Save the picture to a temporary file for printing
-	from tempfile import mktemp
-	im = Image.new('RGB', (683, 384))
-	im.paste(Image.open(image_path).resize((683, 384)), ( 0, 0, 683, 384))
-	output = mktemp(prefix='jpg')
-	im.save(output, format='jpeg')
+    # Save the picture to a temporary file for printing
+    from tempfile import mktemp
+    im = Image.new('RGB', (683, 384))
+    im.paste(Image.open(image_path).resize((683, 384)), ( 0, 0, 683, 384))
+    output = mktemp(prefix='jpg')
+    im.save(output, format='jpeg')
 
-	# Send the picture to the printer
-	print_id = cups.printFile(photo_printer_name, output, "Photo Booth", {})
+    # Send the picture to the printer
+    print_id = cups.printFile(photo_printer_name, output, "Photo Booth", {})
 
-	# Wait until the job finishes
-	from time import sleep
-	while conn.getJobs().get(print_id, None):
-	    sleep(1)
+    # Wait until the job finishes
+    from time import sleep
+    while conn.getJobs().get(print_id, None):
+        sleep(1)
 
-	print("yo finished")
+    print("yo finished")
 
 
 def print_text(text="yoyoyoyoyo"):
-	ser = serial.Serial("/dev/serial0", baudrate=19200)
-	ser.write(bytes(text + "\n", 'ascii'))
+    ser = serial.Serial("/dev/serial0", baudrate=19200)
+    ser.write(bytes(text + "\n", 'ascii'))
 
 # print_image()
 # print_text()
